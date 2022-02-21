5d7d14491ac8f731756e1526b71ed145e515dacc
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-02-21 17:29:36 -0600

    quiet docker output (allow stderr)

--- build-test.sh
+++ build-test.sh
@@ -5,25 +5,26 @@ set -e
 base_tag='npm-cli-socket-bug'
 
 attempt_builds() {
-  dockerfile="$1"
-  tag="$2"
+  build_count="$1"
+  dockerfile="$2"
+  tag="$3"
 
   echo "running 20 build attempts of Dockerfile [$dockerfile] [$tag] on host [$(uname)]"
 
-  for build_attempt in {0..20}; do
+  for build_attempt in {0..$build_count}; do
     echo "build attempt: [${build_attempt}]"
-    docker build --no-cache -t "$tag" -f "$dockerfile" .
+    docker build --no-cache -t "$tag" -f "$dockerfile" . 1> /dev/null
+    echo "build attempt: [${build_attempt}] completed without failure"
   done
-  echo "completed 20 build attempts without failure"
+
+  echo "completed [$build_count] build attempts without failure"
 }
 
 run() {
-  host="$1"
-  echo "running build tests on host [$host]"
 
   attempt_builds Dockerfile.upgrade "$base_tag:upgrade-8.5.1"
   # attempt_builds Dockerfile.downgrade "$base_tag:downgrade-8.3"
   # attempt_builds Dockerfile "$base_tag:no-8.3"
 }
 
-run "$(uname)"
+run
